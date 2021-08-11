521c2aad55bb19d91f6cc1a598ef0c12fe0cca5e
consistentinquiry <62025624+consistentinquiry@users.noreply.github.com>
2021-08-11 20:08:00 +0100

    Update __init__.py

--- whatsin/__init__.py
+++ whatsin/__init__.py
@@ -5,8 +5,8 @@ from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '70dcb28ffcb20a5c76cbd7dd60b02050'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:johnsmells7@whatsin-db.cubhsslyixfb.eu-west-2.rds.amazonaws.com/whatsin_db'
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@whatsin-db.cubhsslyixfb.eu-west-2.rds.amazonaws.com/whatsin_db'
 app.config['UPLOADED_PHOTOS_DEST'] = 'whatsin/static/recipt_uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'gif', 'jpeg' }
 
@@ -22,7 +22,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 
-from whatsin import routes   #wtf is going on here
+from whatsin import routes   #
 from whatsin import watcher  #would this be the correct way of creating a watcher object,
                             #it needs to be accessed throughout the application 
 
